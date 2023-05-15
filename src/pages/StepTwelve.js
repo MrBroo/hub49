d8e9f6491d051209ae10093a8b90c42c9bb20596
Ibrohim Fayzullayev <abi@Abrors-MacBook-Pro.local>
2023-05-15 13:00:19 +0500

    fixed some isseus

--- src/pages/StepTwelve.js
+++ src/pages/StepTwelve.js
@@ -6,7 +6,39 @@ import PostSlider from "../modules/stepEight/PostSlider";
 import { POPPINS } from "../utils/config";
 import downloadIcon from "../assets/icons/download.svg";
 
+import frame1 from "../assets/frame1.svg";
+import frame2 from "../assets/frame2.svg";
+import frame3 from "../assets/frame3.svg";
+import frame4 from "../assets/frame4.svg";
+import frame5 from "../assets/frame5.svg";
+import frame6 from "../assets/frame6.svg";
+import frame7 from "../assets/frame7.svg";
+import frame8 from "../assets/frame8.svg";
+
 export default function StepTwelve() {
+  const data = [
+    { id: 1, img: frame1 },
+    { id: 2, img: frame2 },
+    { id: 3, img: frame3 },
+    { id: 4, img: frame4 },
+    { id: 5, img: frame5 },
+    { id: 6, img: frame6 },
+    { id: 7, img: frame7 },
+    { id: 8, img: frame5 },
+    { id: 9, img: frame8 },
+    { id: 10, img: frame3 },
+    { id: 11, img: frame1 },
+    { id: 12, img: frame2 },
+    { id: 13, img: frame3 },
+    { id: 14, img: frame4 },
+    { id: 15, img: frame5 },
+    { id: 16, img: frame6 },
+    { id: 17, img: frame7 },
+    { id: 18, img: frame5 },
+    { id: 19, img: frame8 },
+    { id: 20, img: frame3 },
+  ];
+
   return (
     <Container>
       <Box sx={{ width: "100%" }}>
@@ -35,7 +67,7 @@ export default function StepTwelve() {
         </Typography>
       </Box>
       <Box>
-        <PostSlider />
+        <PostSlider data={data} />
       </Box>
       <Box
         sx={{
